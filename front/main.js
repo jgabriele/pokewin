5e0cc75264145c6c825046cec2d73bb59e0b4793
Jeremy Gabriele <jeremy.gabriele@corp.badoo.com>
2016-09-06 20:00:50 +0100

    Export counters data in PokeUtils

--- front/main.js
+++ front/main.js
@@ -3,6 +3,7 @@ import Polyfills from './scripts/Polyfills';
 import LocaleManager  from './scripts/LocaleManager';
 import Preloader      from './scripts/Preloader';
 import Utils          from './scripts/Utils';
+import PokeUtils      from './scripts/Utils/PokeUtils';
 
 import ListView             from './scripts/Views/ListView';
 import CounterView          from './scripts/Views/CounterView';
@@ -20,10 +21,6 @@ const NAVIGATOR_LANG_TO_LANG = {
 };
 
 const MINIUM_MOVE_EFFICIENCY_REQUIRED = 1.25;
-
-const RATIO_EFFICIENT = 1.25;
-const RATIO_WEAK = 0.8;
-
 const _state = {};
 
 function _findById(list, id) {
@@ -47,111 +44,7 @@ function _findMoveById(id) {
 
 // Pokemon counters
 
-// Is type1 efficient against type2
-function _isTypeEfficient(type1, type2) {
-  return type1.damages.double.to.indexOf(type2.id) !== -1; // type1 does 1.25 damages on type2
-}
 
-function _isTypeWeak(type1, type2) {
-  return type1.damages.half.to.indexOf(type2.id) !== -1 || // type1 does 0.8 damages on type2
-    type1.damages.no.to.indexOf(type2.id) !== -1; // no === half in PokeGo
-}
-
-function _isSTAB(move, pokemon) {
-  return (pokemon.types.map(t => t.id).indexOf(move.type.id) !== -1);
-}
-
-function _calculateMoveEfficiency(attackPokemon, attackMove, defenseTypes) {
-  // Efficiency of attack pokemon move against defensePokemon
-  let attackEfficiency = 1;
-
-  // STAB
-  if (_isSTAB(attackMove, attackPokemon)) {
-    attackEfficiency *= 1.25;
-  }
-
-  defenseTypes.forEach((defenseType) => {
-    if (_isTypeEfficient(attackMove.type, defenseType)) {
-      attackEfficiency *= RATIO_EFFICIENT;
-    } else if (_isTypeWeak(attackMove.type, defenseType)) {
-      attackEfficiency *= RATIO_WEAK;
-    }
-  });
-
-  return attackEfficiency;
-}
-
-// Tell the type efficiency of one attack with one move against a defense Pokemon
-// Average is made against all possible defense pokemon moves as we never
-// know which move a defense pokemon has
-function _calculatePokemonEfficiency(attackPokemon, attackPokemonMove, defensePokemon, defensePokemonMoves) {
-  const attackEfficiency = _calculateMoveEfficiency(
-    attackPokemon,
-    attackPokemonMove,
-    defensePokemon.types
-  );
-
-  // Efficiency of defense pokemon moves against attackPokemon
-  let index = 0; // Used to calculate average on the fly
-  const defenseEfficiency = defensePokemonMoves.reduce((state, defensePokemonMove) => {
-    const oldIndex = index++;
-    const currentMoveEfficiency = _calculateMoveEfficiency(
-      defensePokemon,
-      defensePokemonMove,
-      attackPokemon.types
-    );
-    return (state * oldIndex + currentMoveEfficiency) / index;
-  }, 1);
-
-  return {
-    move: attackPokemonMove.id,
-    efficiency: (attackEfficiency / defenseEfficiency).toFixed(3)
-  };
-}
-
-// Return list of tuples { move, efficiency } where efficiency is attackEff / defenseEff
-// attackEff is efficiency of attackPokemon's move against defensePokemon type
-// defenseEff is efficiency of each defensePokemon moveS against attackPokemon type
-function _getMovesEfficiency(attackPokemon, defensePokemon) {
-  const attackPokemonMoves = attackPokemon.moves.quick.concat(attackPokemon.moves.charge);
-  const defensePokemonMoves = defensePokemon.moves.quick.concat(defensePokemon.moves.charge);
-
-  return attackPokemonMoves
-    .map((move) => {
-      return _calculatePokemonEfficiency(
-        attackPokemon,
-        move,
-        defensePokemon,
-        defensePokemonMoves
-      );
-    })
-    .filter((moveEfficiency) => {
-      return moveEfficiency.efficiency >= MINIUM_MOVE_EFFICIENCY_REQUIRED;
-    });
-}
-
-
-
-
-
-
-
-function _getCounters(pokemon, otherPokemon) {
-  const moves = _getMovesEfficiency(otherPokemon, pokemon);
-
-  if (!moves.length) {
-    return null;
-  }
-
-  return {
-    id: otherPokemon.id,
-    key: otherPokemon.key,
-    move: _findMoveById(moves[0].move),
-    efficiency: moves[0].efficiency,
-    cpMax: otherPokemon.cpMax,
-    pokemon: otherPokemon // Keep trace of initial pokemon data (used for onClick)
-  };
-}
 
 
 
@@ -180,11 +73,12 @@ function _augmentPokemonsData(pokemons) {
 
 function updateDetail(pokemon) {
   const counters = pokemonsFull
-    .map(_getCounters.bind(null, pokemon))
+    .map(PokeUtils.getCounters.bind(null, MINIUM_MOVE_EFFICIENCY_REQUIRED, pokemon))
     .filter(p => p) // Filter null efficiencies
     .map((counter) => {
-      const moveName = LocaleManager.getInstance().translate(counter.move.key);
-      const moveType = counter.move.type;
+      const move = _findMoveById(counter.move.move);
+      const moveName = LocaleManager.getInstance().translate(move.key);
+      const moveType = move.type;
       const fontSize = Utils.getFontSize(moveName, 70);
       const cp = Math.round(2400 / counter.efficiency);
 
@@ -360,12 +254,9 @@ function _startup () {
       languageSelectView.render();
       languageSelectView.selectLanguage(NAVIGATOR_LANG_TO_LANG[browserLang]);
 
-      window.__localeManager = localeManager;
-
       pokemonsFull = _augmentPokemonsData(pokemons);
       const listView = new ListView()
         .on(ListView.EVENTS.POKEMON_SELECTED, _onPokemonSelected);
-
       listView.render(pokemonsFull);
 
       _addKeyboardListener();
@@ -383,10 +274,14 @@ function _startup () {
         localStorage.setItem(NB_VISITS_KEY, ++nbVisits);
       }
 
+      // Preload the high res spritesheet now that the page has loaded
       preloader.preloadImage(`${location.origin}/images/pokemon-spritesheet.png`)
         .then(_removeLoadingState);
 
       setTimeout(_hideLoading, 600);
+
+      // debug
+      window.__localeManager = localeManager;
     })
     .catch((err) => console.error.bind(console))
 }
---
+++ front/scripts/Utils/PokeUtils.js
@@ -0,0 +1,105 @@
+const RATIO_EFFICIENT = 1.25;
+const RATIO_WEAK = 0.8;
+
+export default {
+  getCounters(efficiencyThreshold, pokemon, otherPokemon){
+    const moves = _getMovesEfficiency(otherPokemon, pokemon, efficiencyThreshold);
+
+    if (!moves.length) {
+      return null;
+    }
+
+    return {
+      id: otherPokemon.id,
+      key: otherPokemon.key,
+      move: moves[0],
+      efficiency: moves[0].efficiency,
+      cpMax: otherPokemon.cpMax,
+      pokemon: otherPokemon // Keep trace of initial pokemon data (used for onClick)
+    };
+  }
+};
+
+
+// Return list of tuples { move, efficiency } where efficiency is attackEff / defenseEff
+// attackEff is efficiency of attackPokemon's move against defensePokemon type
+// defenseEff is efficiency of each defensePokemon moveS against attackPokemon type
+function _getMovesEfficiency(attackPokemon, defensePokemon, efficiencyThreshold) {
+  const attackPokemonMoves = attackPokemon.moves.quick.concat(attackPokemon.moves.charge);
+  const defensePokemonMoves = defensePokemon.moves.quick.concat(defensePokemon.moves.charge);
+
+  return attackPokemonMoves
+    .map((move) => {
+      return _calculatePokemonEfficiency(
+        attackPokemon,
+        move,
+        defensePokemon,
+        defensePokemonMoves
+      );
+    })
+    .filter((moveEfficiency) => {
+      return moveEfficiency.efficiency >= efficiencyThreshold;
+    });
+}
+
+// Tell the type efficiency of one attack with one move against a defense Pokemon
+// Average is made against all possible defense pokemon moves as we never
+// know which move a defense pokemon has
+function _calculatePokemonEfficiency(attackPokemon, attackPokemonMove, defensePokemon, defensePokemonMoves) {
+  const attackEfficiency = _calculateMoveEfficiency(
+    attackPokemon,
+    attackPokemonMove,
+    defensePokemon.types
+  );
+
+  // Efficiency of defense pokemon moves against attackPokemon
+  let index = 0; // Used to calculate average on the fly
+  const defenseEfficiency = defensePokemonMoves.reduce((state, defensePokemonMove) => {
+    const oldIndex = index++;
+    const currentMoveEfficiency = _calculateMoveEfficiency(
+      defensePokemon,
+      defensePokemonMove,
+      attackPokemon.types
+    );
+    return (state * oldIndex + currentMoveEfficiency) / index;
+  }, 1);
+
+  return {
+    move: attackPokemonMove.id,
+    efficiency: (attackEfficiency / defenseEfficiency).toFixed(3)
+  };
+}
+
+function _calculateMoveEfficiency(attackPokemon, attackMove, defenseTypes) {
+  // Efficiency of attack pokemon move against defensePokemon
+  let attackEfficiency = 1;
+
+  // STAB
+  if (_isSTAB(attackMove, attackPokemon)) {
+    attackEfficiency *= 1.25;
+  }
+
+  defenseTypes.forEach((defenseType) => {
+    if (_isTypeEfficient(attackMove.type, defenseType)) {
+      attackEfficiency *= RATIO_EFFICIENT;
+    } else if (_isTypeWeak(attackMove.type, defenseType)) {
+      attackEfficiency *= RATIO_WEAK;
+    }
+  });
+
+  return attackEfficiency;
+}
+
+
+function _isTypeEfficient(type1, type2) {
+  return type1.damages.double.to.indexOf(type2.id) !== -1; // type1 does 1.25 damages on type2
+}
+
+function _isTypeWeak(type1, type2) {
+  return type1.damages.half.to.indexOf(type2.id) !== -1 || // type1 does 0.8 damages on type2
+    type1.damages.no.to.indexOf(type2.id) !== -1; // no === half in PokeGo
+}
+
+function _isSTAB(move, pokemon) {
+  return (pokemon.types.map(t => t.id).indexOf(move.type.id) !== -1);
+}
